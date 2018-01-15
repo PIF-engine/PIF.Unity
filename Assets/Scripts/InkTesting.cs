7203458dd70378add1c68fecd705017aac0b9a0a
dgco2 <dgco2@hotmail.com>
2018-01-15 13:42:09 +0200

    Pre cleanup commit, just in case

--- Assets/Scripts/InkTesting.cs
+++ Assets/Scripts/InkTesting.cs
@@ -14,12 +14,12 @@ public class InkTesting : MonoBehaviour {
     //[SerializeField]
     public Canvas canvas;
 
-   // [SerializeField]
-    private GameObject TMP;
+    //private GameObject TMP;
 
-    private TextMeshProUGUI m_Text;
+    //private TextMeshProUGUI m_Text;
 
-    Camera cam;
+    //[SerializedField]
+    public Camera cam;
 
 
     //For Snippet
@@ -33,6 +33,7 @@ public class InkTesting : MonoBehaviour {
 
        // m_Text = canvas.GetComponentInChildren<TextMeshProUGUI>(); 
         StartStory();
+        
 	}
 	
 	// Update is called once per frame
@@ -56,56 +57,10 @@ public class InkTesting : MonoBehaviour {
         textComp.text = text;
 
 
+       // m_Text.text = text;
         
-    }
 
-
-    //test snippet
-
-    void PrintPos()
-    {
-        string text = textComp.text;
-
-        if (charIndex >= text.Length)
-            return;
-
-        TextGenerator textGen = new TextGenerator(text.Length);
-        Vector2 extents = textComp.gameObject.GetComponent<RectTransform>().rect.size;
-        textGen.Populate(text, textComp.GetGenerationSettings(extents));
-
-        int newLine = text.Substring(0, charIndex).Split('\n').Length - 1;
-        int whiteSpace = text.Substring(0, charIndex).Split(' ').Length - 1;
-        int indexOfTextQuad = (charIndex * 4) + (newLine * 4) - 4;
-        if (indexOfTextQuad < textGen.vertexCount)
-        {
-            Vector3 avgPos = (textGen.verts[indexOfTextQuad].position +
-                textGen.verts[indexOfTextQuad + 1].position +
-                textGen.verts[indexOfTextQuad + 2].position +
-                textGen.verts[indexOfTextQuad + 3].position) / 4f;
-
-            print(avgPos);
-            PrintWorldPos(avgPos);
-        }
-        else
-        {
-            Debug.LogError("Out of text bound");
-        }
-    }
-
-    void PrintWorldPos(Vector3 testPoint)
-    {
-        Vector3 worldPos = textComp.transform.TransformPoint(testPoint);
-        print(worldPos);
-        new GameObject("point").transform.position = worldPos;
-        Debug.DrawRay(worldPos, Vector3.up, Color.red, 50f);
-    }
-
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 100, 80), "Test"))
-        {
-            PrintPos();
-        }
+        
     }
 
 }
---
+++ Assets/Scripts/TMPTesting.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TMPTesting : MonoBehaviour {
+
+
+    //Public Serializables
+    public Camera cam;
+
+    //public TextMeshProUGUI m_Test;
+
+    public Canvas parent;
+    public GameObject startPoint;
+
+    public GameObject textPrefab;
+
+
+
+    //Private
+    string sentance;
+
+
+	// Use this for initialization
+	void Start () {
+        sentance = "test words in the sentance with text mesh pro! I am going to write a run on sentance to test out how well the EOL works!";
+        splitTextRect();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    
+    void splitTextRect()
+    {
+        Vector3 parentPos = parent.transform.position;
+        List<string> words = new List<string>(sentance.Split(' '));
+
+        Vector3 offset = new Vector3(0,0,0);
+
+        Transform trans = startPoint.transform;
+
+        //Debug vars and assignments
+        var pX = parent.GetComponent<RectTransform>().sizeDelta.x; //Get width of canvas
+        var sX = startPoint.transform.localPosition.x; // figure out where the start point is from the center
+        sX += pX / 2; // This number gives us our right margin
+        sX *= 2; //margin test
+        var EOLoffset = pX - sX; //Subtract them to get our EOL number
+        Debug.Log(pX + " - " + sX + " = " + EOLoffset);
+
+
+        bool firstWord = true;
+        foreach (string s in words)
+        {
+            var word = Instantiate(textPrefab);
+            word.transform.SetParent(trans);
+            word.transform.localScale = new Vector3(1, 1, 1);
+            
+            word.transform.localPosition = offset;
+
+
+
+            //Debug.Log(word.transform.parent);
+            //Debug.Log(parent.transform);
+
+
+
+
+
+            //TextMeshProUGUI m_Text = word.AddComponent(typeof(TextMeshProUGUI)) as TextMeshProUGUI;
+            //HorizontalLayoutGroup layout = word.AddComponent(typeof(HorizontalLayoutGroup)) as HorizontalLayoutGroup;
+            //ContentSizeFitter fitter = word.AddComponent(typeof(ContentSizeFitter)) as ContentSizeFitter;
+
+            //fitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            //fitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+
+            TextMeshProUGUI m_Text = word.GetComponent<TextMeshProUGUI>();
+
+            m_Text.text = firstWord ? s : " " + s;
+            if (firstWord) firstWord = false;
+            
+            m_Text.ForceMeshUpdate();
+
+            //fitter.
+            RectTransform rect = word.GetComponent<RectTransform>();
+            
+
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+
+            //debug print of offset
+             //Debug.Log("Offset for word: " + m_Text.text + " is " +offset.x);
+
+            if (offset.x+ rect.sizeDelta.x >= EOLoffset)
+            {
+                Debug.Log("This word should be on a new line");
+                offset.x = 0;
+                offset.y -= 50; //magic number, calc later
+                word.transform.localPosition = offset;
+                m_Text.text = s;
+                LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+
+            }
+
+
+
+
+
+            // move offset (Probably)
+            // find way to set x pivot to zero before offset update with width!
+            //rect.pivot.Set(0, rect.pivot.y);
+            offset.x += rect.sizeDelta.x;
+            
+            
+
+            //Debug.Log(rect.sizeDelta.x);
+            //offset.x += word.GetComponent<RectTransform>().rect.width * parent.scaleFactor / 2;
+
+            // wordPos.y += (wordPos.y - oldPos.y);
+
+            //parent test
+            //trans = word.transform;
+
+            //word
+        }
+    }
+}
