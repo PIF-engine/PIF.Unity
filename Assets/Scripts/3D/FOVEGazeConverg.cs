7be5cb409d7b184ec413481e8bd5df0bb615f8df
PTSD\dgco2 <dgco2@hotmail.com>
2018-04-16 12:18:15 +0300

    Added a gaze debugger so we can track where the user is looking. Needs to be tested on the FOVE itself

---
+++ Assets/Scripts/3D/FOVEGazeConverg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVEGazeConverg : MonoBehaviour {
+
+
+    public GameObject FOVERig;
+    private FoveInterface rig;
+    public bool active;
+
+
+
+	// Use this for initialization
+	void Start () {
+        if (!active || FOVERig == null)
+            //if we're not active, destroy the parent
+            Destroy(gameObject);
+
+        rig = FOVERig.GetComponentInChildren<FoveInterface>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FoveInterfaceBase.GazeConvergenceData dat;
+        try
+        {
+            dat = rig.GetWorldGazeConvergence();
+        } catch (Exception e)
+        {
+            Debug.Log("Error getting Gaze data! Destroying the debugger...");
+            Destroy(gameObject);
+            return;
+        }
+        Vector3 endpoint = dat.ray.origin + (dat.ray.direction * dat.distance);
+        gameObject.transform.position = endpoint;
+
+	}
+}
