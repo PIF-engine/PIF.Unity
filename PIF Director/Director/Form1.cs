f3fcfea40708f47b0553b9622c070ccfbcb5eb99
jfrey <nomhad@gmail.com>
2018-08-22 20:35:21 +0200

    LSL: switch marker stream to Markers type

--- PIF Director/Director/Form1.cs
+++ PIF Director/Director/Form1.cs
@@ -111,7 +111,7 @@ namespace Director
 
         public void ProcessChoiceMarker()
         {
-            liblsl.StreamInfo[] results = liblsl.resolve_stream("type", "Choice.Request");
+            liblsl.StreamInfo[] results = liblsl.resolve_stream("type", "Markers");
             liblsl.StreamInlet inlet = new liblsl.StreamInlet(results[0]);
 
             string[] sample = new string[1];
